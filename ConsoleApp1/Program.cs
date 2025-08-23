350ddae099f51c2226b0cb4b4a81e27114325d9d
jusu <erikvixx@gmail.com>
2025-08-23 11:34:36 -0300

    ex 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DivideAndConquest
+{
+    public class DivideAndConquest
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = [2, 4, 6];
+
+            int result = SumDNC(array);
+            Console.WriteLine(result);
+        }
+        public static int SumDNC(int[] arr, int index = 0)
+        {
+            if (index >= arr.Length)
+            {
+                return 0;
+            }
+            return arr[index] + SumDNC(arr, index + 1);
+        }
+    }
+}
